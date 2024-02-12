1a7bb774751046eae7d8241071340ef7db9a510e
Richard Qian <rqian@ufl.edu>
2024-02-12 16:46:58 -0500

    Add routing to different pages

--- dash/dash_app.py
+++ dash/dash_app.py
@@ -1,9 +1,14 @@
+import dash
+import dash_bootstrap_components as dbc
+
 from dash import Dash, html
 
-app = Dash(__name__)
+app = Dash(__name__, external_stylesheets=[dbc.themes.JOURNAL], use_pages=True)
+app.title = 'RunTime'
 
 app.layout = html.Div([
-    html.Div(children='Hello World')
+    html.Div(children='Hello World'),
+    dash.page_container
 ])
 
 if __name__ == '__main__':
