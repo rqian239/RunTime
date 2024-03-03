c9263f30ed22e31b2b0852a873184679178ae26c
Richard Qian <richardqiancs@gmail.com>
2024-03-03 15:33:23 -0500

    Fixed ModuleNotFoundError when trying to import modules

--- dash/app/dash_app.py
+++ dash/app/dash_app.py
@@ -1,5 +1,17 @@
 # Main entry point for Dash
 
+# --------------------------------------------------------
+import sys
+from pathlib import Path
+
+# This is needed so that imports work in this project
+# Not best practice but it makes files within the dash/ directory visible by Python interpreter
+
+src_dir = Path(__file__).resolve().parent.parent
+# print(src_dir)
+sys.path.append(str(src_dir))
+# --------------------------------------------------------
+
 import dash
 import dash_bootstrap_components as dbc
 
