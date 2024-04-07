dd6dad1dc2ad540b3335ec1199ba466d15d12695
Richard Qian <richardqiancs@gmail.com>
2024-04-06 21:48:29 -0400

    Remove nav and ftr from main file, refactor the font styling

--- src/app.py
+++ src/app.py
@@ -18,24 +18,18 @@ import dash
 import dash_bootstrap_components as dbc
 
 from dash import Dash, html
-from components.navbar import navbar_simple
-from components.footer import footer
 
 # Define the app here and choose the DBC theme
 # Themes listed here: https://dash-bootstrap-components.opensource.faculty.ai/docs/themes/
 app = Dash(__name__, external_stylesheets=[dbc.themes.VAPOR], use_pages=True)
 app.title = 'RunTime'
 
-nav = navbar_simple()
-ftr = footer()
-
 app.layout = html.Div(
-    style={'margin': '0', 'padding': '0', 'font-family': 'Arial, sans-serif'},
+    className="project_font",
     children=[
-        html.Div([nav]),        # This will show up for each page
         dash.page_container,    # Page specific layouts
-        html.Div([ftr])
-])
+    ]
+)
 
 if __name__ == '__main__':
     app.run(debug=True)
