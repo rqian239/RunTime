72f4e7036f97b463a8891d214e8cbe1458272660
NNatarajan0717 <nnatarajan@ufl.edu>
2024-04-09 18:31:30 -0400

    added descriptions to about page

--- src/pages/about_page.py
+++ src/pages/about_page.py
@@ -31,41 +31,16 @@ body = dbc.Container(
                         html.Br(),
                         html.P(
                             """\
-                            Welcome to the About Us Page!""",
+                           Thank you for visiting our About Us page. Here, we'd like to introduce you to the team behind our project 
+                           and provide insights into our mission and vision. Get to know the talented individuals who have contributed 
+                           their expertise and passion to make this project possible.
+                            """,
                             style={'textAlign': 'center'}
                         ),
                     ],
                     width=12  # Full width for this column
                 )
             ]
-        ),
-        dbc.Row(
-            [
-                # Adding the first card with dropdown
-                dbc.Col(
-                    [
-                        dbc.Card(
-                            [
-                                dbc.CardHeader("Card 1"),
-                                dbc.CardBody(
-                                    [
-                                        html.H4("Card title", className="card-title"),
-                                        html.P(
-                                            "This is some text inside the card body. You can add any content you want here.",
-                                            className="card-text"
-                                        )
-                                    ]
-                                )
-                            ],
-                            className="card text-white bg-primary mb-3",
-                            style={"maxWidth": "80rem", "width": "100%", "margin": "auto"}  # Adjust the maxWidth and width here
-                        ),
-                    ],
-                    width=15,  # 100% width for this column
-                    align='center'  # Center align the column content
-                )
-            ],
-            justify="center"  # Center align the row
         ),
         dbc.Row(
             [
@@ -77,6 +52,7 @@ body = dbc.Container(
                 # Adding a new row for small cards
                 dbc.Col(
                     [
+                        html.Br(),
                         dbc.Card(
                             [
                                 dbc.CardHeader("Front-End Developer"),
@@ -99,6 +75,7 @@ body = dbc.Container(
                 ),
                 dbc.Col(
                     [
+                        html.Br(),
                         dbc.Card(
                             [
                                 dbc.CardHeader("Front-End Developer"),
@@ -120,6 +97,7 @@ body = dbc.Container(
                 ),
                 dbc.Col(
                     [
+                        html.Br(),
                         dbc.Card(
                             [
                                 dbc.CardHeader("Back-End Developer"),
@@ -140,6 +118,7 @@ body = dbc.Container(
                 ),
                 dbc.Col(
                     [
+                        html.Br(),
                         dbc.Card(
                             [
                                 dbc.CardHeader("Back-End Developer"),
@@ -161,6 +140,7 @@ body = dbc.Container(
                 ),
                 dbc.Col(
                     [
+                        html.Br(),
                         dbc.Card(
                             [
                                 dbc.CardHeader("Back-End Developer"),
@@ -182,6 +162,175 @@ body = dbc.Container(
             ],
             justify="center"  # Center align the row
         ),
+        dbc.Row(
+    [
+        # Adding the first card with dropdown
+        dbc.Col(
+            [
+                dbc.Card(
+                    [
+                        dbc.CardHeader("Nivedha Natarajan"),
+                        dbc.CardBody(
+                            [
+                                html.P(
+                                    """
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. 
+                                    Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. 
+                                    Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris 
+                                    massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent 
+                                    per conubia nostra, per inceptos himenaeos.
+                                    """,
+                                    className="card-text"
+                                )
+                            ]
+                        )
+                    ],
+                    className="card text-white bg-primary mb-3",
+                    style={"maxWidth": "80rem", "width": "100%", "margin": "auto"}  # Adjust the maxWidth and width here
+                ),
+            ],
+            width=15,  # 100% width for this column
+            align='center'  # Center align the column content
+        )
+    ],
+    justify="center"  # Center align the row
+),
+dbc.Row(
+    [
+        # Adding the first card with dropdown
+        dbc.Col(
+            [
+                dbc.Card(
+                    [
+                        dbc.CardHeader("Nikitha Chintalapati"),
+                        dbc.CardBody(
+                            [
+                                html.P(
+                                    """
+                                    Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. 
+                                    Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed 
+                                    convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus 
+                                    risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula 
+                                    lacinia aliquet. Mauris ipsum.
+                                    """,
+                                    className="card-text"
+                                )
+                            ]
+                        )
+                    ],
+                    className="card text-white bg-primary mb-3",
+                    style={"maxWidth": "80rem", "width": "100%", "margin": "auto"}  # Adjust the maxWidth and width here
+                ),
+            ],
+            width=15,  # 100% width for this column
+            align='center'  # Center align the column content
+        )
+    ],
+    justify="center"  # Center align the row
+),
+dbc.Row(
+    [
+        # Adding the first card with dropdown
+        dbc.Col(
+            [
+                dbc.Card(
+                    [
+                        dbc.CardHeader("Bryan Hernandez"),
+                        dbc.CardBody(
+                            [
+                                html.P(
+                                    """
+                                    Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat 
+                                    condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, 
+                                    per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor 
+                                    neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. 
+                                    Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. 
+                                    Proin quam. Etiam ultrices.
+                                    """,
+                                    className="card-text"
+                                )
+                            ]
+                        )
+                    ],
+                    className="card text-white bg-primary mb-3",
+                    style={"maxWidth": "80rem", "width": "100%", "margin": "auto"}  # Adjust the maxWidth and width here
+                ),
+            ],
+            width=15,  # 100% width for this column
+            align='center'  # Center align the column content
+        )
+    ],
+    justify="center"  # Center align the row
+),
+dbc.Row(
+    [
+        # Adding the first card with dropdown
+        dbc.Col(
+            [
+                dbc.Card(
+                    [
+                        dbc.CardHeader("Ria Manoj Gandhi"),
+                        dbc.CardBody(
+                            [
+                                html.P(
+                                    """
+                                    Suspendisse in justo eu magna luctus suscipit. Sed lectus. Integer euismod lacus luctus 
+                                    magna. Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum 
+                                    magna augue eget diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices 
+                                    posuere cubilia Curae; Morbi lacinia molestie dui. Praesent blandit dolor. Sed non quam. 
+                                    In vel mi sit amet augue congue elementum. Morbi in ipsum sit amet pede facilisis laoreet. 
+                                    Donec lacus nunc, viverra nec, blandit vel, egestas et, augue. Vestibulum tincidunt 
+                                    malesuada tellus. Ut ultrices ultrices enim.
+                                    """,
+                                    className="card-text"
+                                )
+                            ]
+                        )
+                    ],
+                    className="card text-white bg-primary mb-3",
+                    style={"maxWidth": "80rem", "width": "100%", "margin": "auto"}  # Adjust the maxWidth and width here
+                ),
+            ],
+            width=15,  # 100% width for this column
+            align='center'  # Center align the column content
+        )
+    ],
+    justify="center"  # Center align the row
+),
+dbc.Row(
+    [
+        # Adding the first card with dropdown
+        dbc.Col(
+            [
+                dbc.Card(
+                    [
+                        dbc.CardHeader("Richard Qian"),
+                        dbc.CardBody(
+                            [
+                                html.P(
+                                    """
+                                    Curabitur sit amet mauris. Morbi in dui quis est pulvinar ullamcorper. Nulla facilisi. 
+                                    Integer lacinia sollicitudin massa. Cras metus. Sed aliquet risus a tortor. Integer 
+                                    id quam. Morbi mi. Quisque nisl felis, venenatis tristique, dignissim in, ultrices 
+                                    sit amet, augue. Proin sodales libero eget ante. Nulla quam. Aenean laoreet. Vestibulum 
+                                    nisi lectus, commodo ac, facilisis ac, ultricies eu, pede. Ut orci risus, accumsan porttitor, 
+                                    cursus quis, aliquet eget, justo.
+                                    """,
+                                    className="card-text"
+                                )
+                            ]
+                        )
+                    ],
+                    className="card text-white bg-primary mb-3",
+                    style={"maxWidth": "80rem", "width": "100%", "margin": "auto"}  # Adjust the maxWidth and width here
+                ),
+            ],
+            width=15,  # 100% width for this column
+            align='center'  # Center align the column content
+        )
+    ],
+    justify="center"  # Center align the row
+)
     ],
     class_name="body-flex-wrapper",
 )
