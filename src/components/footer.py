8e8f264e92140a9eef86ce353df83b4fef38193b
NNatarajan0717 <nnatarajan@ufl.edu>
2024-04-14 11:09:12 -0400

    footer final

--- src/components/footer.py
+++ src/components/footer.py
@@ -1,4 +1,3 @@
-import dash
 import dash_bootstrap_components as dbc
 from dash import html
 
@@ -11,35 +10,25 @@ def footer():
                 dbc.Row(
                     [
                         dbc.Col(
-                            html.Div([
-                                html.H4("Quick Links", className="text-uppercase"),
-                                # html.Div([
-                                #     dbc.NavLink("Home", href="/", className="footer-link d-inline-flex"),
-                                #     dbc.NavLink("About", href="/about", className="footer-link d-inline-flex"),
-                                #     dbc.NavLink("Teams", href="/teams", className="footer-link d-inline-flex"),
-                                #     dbc.NavLink("Upcoming Games", href="/upcominggames", className="footer-link d-inline-flex"),
-                                #     dbc.NavLink("Fantasy Games", href="/fantasygames", className="footer-link d-inline-flex")
-                                # ], className="d-flex flex-row justify-content-around"),  # Adjust justify-content as needed
-                            ]),
-                            class_name="text-center mt-3"
+                            [
+                                html.H4("Quick Links", className="text-uppercase text-white"),
+                                html.Div([
+                                    dbc.Button("Home", href="/", color="primary", className="btn btn-primary btn-link mr-2"),
+                                    dbc.Button("About", href="/about", color="primary", className="btn btn-primary btn-link mr-2"),
+                                    dbc.Button("Teams", href="/teams", color="primary", className="btn btn-primary btn-link mr-2"),
+                                    dbc.Button("Upcoming Games", href="/upcominggames", color="primary", className="btn btn-primary btn-link mr-2"),
+                                    dbc.Button("Fantasy Games", href="/fantasygames", color="primary", className="btn btn-primary btn-link mr-2"),
+                                ], className="btn-group"),  # Adding btn-group class to make buttons horizontal
+                            ],
+                            className="text-center mt-3"
                         ),
                     ]
                 ),
                 dbc.Row(
                     [
-                        dbc.Col(dbc.Button("Home", href="/", class_name="btn-link")),
-                        dbc.Col(dbc.Button("About", href="/about", class_name="btn-link")),
-                        dbc.Col(dbc.Button("Teams", href="/teams", class_name="btn-link")),
-                        dbc.Col(dbc.Button("Upcoming Games", href="/upcominggames", class_name="btn-link")),
-                        dbc.Col(dbc.Button("Fantasy Games", href="/fantasygames", class_name="btn-link")),
-                    ],
-                    class_name="text-center g-1 mb-4"
-                ),
-                dbc.Row(
-                    [
                         dbc.Col(
                             html.Div([
-                                html.H5("Created by the Runtime Terrors", className="text-center text-italic"),
+                                html.H5("Created by the Runtime Terrors", className="text-center text-italic text-white"),
                             ]),
                         ),
                     ],
@@ -47,21 +36,10 @@ def footer():
                 ),
                 dbc.Row(
                     [
-                        # dbc.Col(
-                        #     html.Div([
-                        #         html.H5("Quick Links", className="text-uppercase"),
-                        #         dbc.NavLink("Home", href="/", className="footer-link"),
-                        #         dbc.NavLink("About", href="/about", className="footer-link"),
-                        #         dbc.NavLink("Teams", href="/teams", className="footer-link"),
-                        #         dbc.NavLink("Upcoming Games", href="/upcominggames", className="footer-link"),
-                        #         dbc.NavLink("Fantasy Games", href="/fantasygames", className="footer-link")
-                        #     ]),
-                        #     className="centered"
-                        # ),
                         dbc.Col(
                             html.Div([
-                                html.P("Nikitha Chintalapati, Ria Gandhi, Bryan Hernandez-Sanchez, Nivedha Natarajan, Richard Qian", className="mb-2"),
-                                html.P("University of Florida 2024 🐊")
+                                html.P("Nikitha Chintalapati, Ria Gandhi, Bryan Hernandez-Sanchez, Nivedha Natarajan, Richard Qian", className="mb-2 text-white"),
+                                html.P("University of Florida 2024 🐊", className="text-white")
                             ]),
                             className="text-center text-italic",
                         ),
@@ -70,6 +48,6 @@ def footer():
             ],
             fluid=True,
         ),
-        className="footer-styling",
+        className="footer-styling bg-light-purple",
     )
     return ftr
